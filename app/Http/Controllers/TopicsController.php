48d606c63c23262d26c499636d45630a2a282932
SmauelL <ljxlm502661058@163.com>
2019-05-31 10:10:34 +0800

    only edit mime

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -85,6 +85,6 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('topics.index')->with('success', '删除成功!');
 	}
 }
